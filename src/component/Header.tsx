f2676ecd426e34e1cde49c237bc235717cfeb73a
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2024-01-16 17:45:20 +0000

    Work on header - Add logo and search bar

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <img
+        src="https://banner2.cleanpng.com/20190418/qty/kisspng-amazon-web-services-logo-cloud-computing-amazon-co-logoaws-1-itnext-summit-5cb80ea9bc03d6.8054658415555662497701.jpg"
+        alt="logo"
+      />
+
+      <input
+        type="text"
+        placeholder="Search products by description or name"
+      ></input>
+    </header>
+  );
 };
 
 export default Header;
