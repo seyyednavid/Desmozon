5e0d59fe60e09d3d9579222ab413c4086e3e7efe
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2024-01-17 18:28:30 +0000

    Fetch data via API and put into product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./component/Header";
 import Products from "./component/Products";
-
+import { products } from "./types";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState<products>([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async (): Promise<void> => {
+    const res = await axios("https://fakestoreapi.com/products");
+    setProducts(res.data);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Products />
+      <Products products={products} />
     </div>
   );
 }
--- src/component/Products.tsx
+++ src/component/Products.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { products } from "../types";
 
-const Products = () => {
-  return <h2>Products</h2>;
+interface ProductsType {
+  products: products;
+}
+
+const Products = ({ products }: ProductsType) => {
+  console.log(products);
+  return (
+    <div className="container">
+      {products.map((product) => (
+        <div className="card">
+          <div className="image-container">
+            <img src={product.image} alt={product.title} />
+          </div>
+          <div className="card-body">
+            <p>{product.title}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Products;
